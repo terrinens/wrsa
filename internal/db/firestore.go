0bf7b3bcd0728191f6be8066dee0d165b5866927
DongChuelKim <terrinens@gmail.com>
2025-10-14 14:28:47 +0900

    feat : db init

---
+++ internal/db/firestore.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go/v4"
+	"google.golang.org/api/option"
+)
+
+var Client *firestore.Client
+
+/*InitClient 해당 함수는 DB를 초기화하고 글로벌 변수 Client에 결과를 주입합니다.*/
+func InitClient() {
+	var clientOptions option.ClientOption
+
+	if os.Getenv("DEV") == "true" {
+		clientOptions = option.WithCredentialsFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+	} else {
+		var credentialsFile, err = os.ReadFile("/keys/firestore_key")
+
+		if err != nil {
+			log.Fatalf("Error reading firestore key file: %v", err)
+		}
+
+		clientOptions = option.WithCredentialsJSON(credentialsFile)
+	}
+
+	app, err := firebase.NewApp(context.Background(), nil, clientOptions)
+	if err != nil || app == nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+
+	client, err := app.Firestore(context.Background())
+	if err != nil || client == nil {
+		log.Fatalf("error getting client: %v\n", err)
+	}
+
+	Client = client
+}
--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"db_sync/internal/db"
+	"log"
+	"os"
+	"strings"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	database.InitClient()
+}
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+func init() {
+	if len(os.Args) > 1 && strings.ToLower(os.Args[1]) == "dev" {
+		err := os.Setenv("DEV", "true")
+		if err != nil {
+			log.Fatalf("Failed to set DEV environment variable")
+		}
 	}
 }
