c460ace43965ba4ffc466bddc06035c01502dc6c
DongChuelKim <terrinens@gmail.com>
2025-10-23 14:56:22 +0900

    feat : 평균 풍속 계산 함수 추가

--- internal/lib/calculate/wind.go
+++ internal/lib/calculate/wind.go
@@ -1,1 +1,17 @@
 package calculate
+
+import (
+	"db_sync/internal/lib/weather_API"
+	"strconv"
+)
+
+func windAvg(wsd []weather_API.VillageFcstItem) float64 {
+	var total float64
+
+	for _, item := range wsd {
+		value, _ := strconv.ParseFloat(item.FcstValue, 64)
+		total += value
+	}
+
+	return total / float64(len(wsd))
+}
